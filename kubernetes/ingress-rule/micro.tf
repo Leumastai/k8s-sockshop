66bf2e2c33b6aace39463af7989cca2f5ef74e4f
samuel <samueltaiwo502@gmail.com>
2024-08-10 21:17:24 +0100

    feat: initial upload

---
+++ kubernetes/ingress-rule/micro.tf
@@ -0,0 +1,30 @@
+resource "kubernetes_ingress_v1" "micro-ingress" {
+  metadata {
+    name      = "sock-shop"
+    namespace = "sock-shop"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "soskshop.wuaze.com"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
