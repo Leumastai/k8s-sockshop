4355936dc688d47698077e86629c77f83694e01b
samuel <samueltaiwo502@gmail.com>
2024-08-12 20:55:46 +0100

    fix: remove route53 and ACM from terraform deployemnt

--- kubernetes/ingress-rule/micro.tf
+++ kubernetes/ingress-rule/micro.tf
@@ -12,7 +12,7 @@ resource "kubernetes_ingress_v1" "micro-ingress" {
 
   spec {
     rule {
-      host = "sockshop-k8s.ng.com"
+      host = "soskshop.wuaze.com"
       http {
         path {
           backend {
--- kubernetes/nginx-controller/variables.tf
+++ kubernetes/nginx-controller/variables.tf
@@ -1,11 +1,11 @@
 variable "domain_name" {
   description = "This is the domain name"
   type        = string
-  default     = "sockshop-k8s.ng.com"
+  default     = "soskshop.wuaze.com"
 }
   
 variable "alt_domain_name" {
   description = "This is the alternative domain name"
   type        = string
-  default     = "sockshop-k8s.ng.com"
+  default     = "soskshop.wuaze.com"
 }
