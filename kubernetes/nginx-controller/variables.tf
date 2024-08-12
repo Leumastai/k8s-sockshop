06fc42ed9b81dabe31b17c21dbefa12c4f67dec5
samuel <samueltaiwo502@gmail.com>
2024-08-12 21:30:36 +0100

    fix: revert

--- kubernetes/nginx-controller/variables.tf
+++ kubernetes/nginx-controller/variables.tf
@@ -1,11 +1,11 @@
 variable "domain_name" {
   description = "This is the domain name"
   type        = string
-  default     = "soskshop.wuaze.com"
+  default     = "sockshop-k8s.ng.com"
 }
   
 variable "alt_domain_name" {
   description = "This is the alternative domain name"
   type        = string
-  default     = "soskshop.wuaze.com"
+  default     = "sockshop-k8s.ng.com"
 }
